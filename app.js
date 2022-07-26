4c44796fd284b1af307071ae0ce0a07f45920e3a
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-07-26 15:45:55 -0700

    add additional JAvaScript Functionality and Css sttyling.

--- app.js
+++ app.js
@@ -6,13 +6,29 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const startCupOne = document.getElementById('cup-one-two');
+const endCupOne = document.getElementById('cup-one-three');
+
+const startCupTwo = document.getElementById('cup-two-two');
+const endCupTwo = document.getElementById('cup-two-three');
+
+const startCupThree = document.getElementById('cup-three-two');
+const endCupThree = document.getElementById('cup-three-three');
+
+const ballOne = document.getElementById('winrar-one');
+const ballTwo = document.getElementById('winrar-two');
+const ballThree = document.getElementById('winrar-three');
+
 const buttonOne = document.getElementById('cup-one');
 const buttonTwo = document.getElementById('cup-two');
 const buttonThree = document.getElementById('cup-three');
+const buttonReset = document.getElementById('reset');
 
 const correctEl = document.getElementById('correct');
 const wrongEl = document.getElementById('wrong');
 const totalEl = document.getElementById('total');
+
+// const cupOneTwo = document.getElementById('cup-one-two');
 
 const cups = ['cupOne', 'cupTwo', 'cupThree'];
 
@@ -29,6 +45,16 @@ function getRandomCup() {
 function checkGuess(answer, userGuess) {
     total++;
     totalEl.textContent = total;
+    
+    
+    if (answer === cups[0]) {
+        ballOne.classList.remove('hidden');
+    } else if (answer === cups[1]) {
+        ballTwo.classList.remove('hidden');
+    } else if (answer === cups[2]) {
+        ballThree.classList.remove('hidden');
+    }
+    
     if (answer === userGuess) {
         correct++;
         correctEl.textContent = correct;
@@ -36,17 +62,45 @@ function checkGuess(answer, userGuess) {
         wrong++;
         wrongEl.textContent = wrong;
     }
+    
+}
+
+
+function disableButtons() {
+    buttonOne.disabled = true;
+    buttonTwo.disabled = true;
+    buttonThree.disabled = true;
 }
 
 buttonOne.addEventListener('click', () => {
     getRandomCup();
-    checkGuess(getRandomCup(), 'cupOne');  
+    checkGuess(getRandomCup(), 'cupOne'); 
+    startCupOne.classList.add('hidden');
+    endCupOne.classList.remove('hidden');
+    disableButtons();
 });
 buttonTwo.addEventListener('click', () => {
     getRandomCup();
     checkGuess(getRandomCup(), 'cupTwo');
+    startCupTwo.classList.add('hidden');
+    endCupTwo.classList.remove('hidden');
+    disableButtons();
 });
 buttonThree.addEventListener('click', () => {
     getRandomCup();
     checkGuess(getRandomCup(), 'cupThree');
+    startCupThree.classList.add('hidden');
+    endCupThree.classList.remove('hidden');
+    disableButtons();
+});
+buttonReset.addEventListener('click', () => {
+    buttonOne.disabled = false;
+    buttonTwo.disabled = false;
+    buttonThree.disabled = false;
+    startCupOne.classList.remove('hidden');
+    endCupOne.classList.add('hidden');
+    startCupTwo.classList.remove('hidden');
+    endCupTwo.classList.add('hidden');
+    startCupThree.classList.remove('hidden');
+    endCupThree.classList.add('hidden');
 });
